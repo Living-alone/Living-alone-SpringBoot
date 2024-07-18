11b248df70c3b7f036e9a7f9b655d8b1e0da63dc
scc9811 <101318479+scc9811@users.noreply.github.com>
2024-07-18 09:36:24 +0900

    Init: Spring Boot 프로젝트 생성
    - Spring Boot 프로젝트 생성
    - 프로젝트와 무관한 로컬 설정파일들 .gitignore 에 추가

---
+++ living-alone-springboot/src/main/java/com/livingalone/springboot/LivingAloneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.livingalone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingAloneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingAloneSpringBootApplication.class, args);
+	}
+
+}
