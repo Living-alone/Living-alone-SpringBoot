c6053c0b24c0b33cac875fe710aedf881060a5fb
scc9811 <wjsrudtjr98@naver.com>
2024-07-25 11:51:17 +0900

    Feat: 채팅 상대 표시 기능 추가
    - Request Body :
    {
     "chatRoomId": 1
     "userId": 1
    }
    
    - Response :
    {
     "userId": 2
    }

--- living-alone-springboot/src/main/java/com/livingalone/springboot/chat/controller/ChatController.java
+++ living-alone-springboot/src/main/java/com/livingalone/springboot/chat/controller/ChatController.java
@@ -1,8 +1,10 @@
 package com.livingalone.springboot.chat.controller;
 
+import com.livingalone.springboot.chat.dto.ChatPartnerRequest;
 import com.livingalone.springboot.chat.entity.ChatRoom;
 import com.livingalone.springboot.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +23,7 @@ public class ChatController {
 
     private final ChatService chatService;
 
+    // 참여중인 채팅방 목록 반환
     @PostMapping("chatRooms")
     public ResponseEntity<List<ChatRoom>> getChatRooms(@RequestBody Map<String, Object> payload) {
         Long userId = ((Number) payload.get("userId")).longValue();
@@ -27,5 +31,14 @@ public class ChatController {
         return ResponseEntity.ok().body(chatRooms);
     }
 
+    // 채팅 상대 userId 반환
+    @PostMapping("chatPartner")
+    public ResponseEntity<Map<String, Long>> getChatPartner(@RequestBody ChatPartnerRequest chatPartnerRequest) {
+        Long userId = chatService.getChatPartnerUserId(chatPartnerRequest);
+        Map<String, Long> response = new HashMap<>();
+        response.put("userId", userId);
+        // 조회 실패 시 ( userId == -1L ) BAD_REQUEST 반환.
+        return ResponseEntity.status(userId == -1 ? HttpStatus.BAD_REQUEST : HttpStatus.ACCEPTED).body(response);
+    }
 
 }
--- living-alone-springboot/src/main/java/com/livingalone/springboot/chat/service/ChatService.java
+++ living-alone-springboot/src/main/java/com/livingalone/springboot/chat/service/ChatService.java
@@ -1,11 +1,14 @@
 package com.livingalone.springboot.chat.service;
 
+import com.livingalone.springboot.chat.dto.ChatPartnerRequest;
 import com.livingalone.springboot.chat.entity.ChatRoom;
 import com.livingalone.springboot.chat.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,4 +24,14 @@ public class ChatService {
         // postAuthorId == userId || postViewerId == userId
         return chatRoomRepository.findByPostAuthorIdOrPostViewerId(userId, userId);
     }
+
+    public Long getChatPartnerUserId(ChatPartnerRequest chatPartnerRequest) {
+        Optional<ChatRoom> optionalChatRoom = chatRoomRepository.findByChatRoomIdAndUserId(chatPartnerRequest.getChatRoomId(), chatPartnerRequest.getUserId());
+        // 요청과 일치하는 채팅방 없는 경우 -1 반환.
+        if(optionalChatRoom.isEmpty()) return -1L;
+        ChatRoom chatRoom = optionalChatRoom.get();
+        // chatRoom의 postAuthorId, postViewerId 중 요청받은 userId와 다른 Id 반환.
+        return Objects.equals(chatRoom.getPostAuthorId(), chatPartnerRequest.getUserId()) ? chatRoom.getPostViewerId() : chatRoom.getPostAuthorId();
+    }
+
 }
