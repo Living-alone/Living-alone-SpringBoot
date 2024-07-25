aa41d4bbe69a248090b1b9fa97caa5312a0c90ba
scc9811 <wjsrudtjr98@naver.com>
2024-07-25 16:26:33 +0900

    Feat: 이전 채팅 목록 표시 기능 추가
    - 예시 url : "ws://~~/chat?userId=1&partnerUserId=2&postId=1"
    - * postId는 optional ( 만약 게시글에서 채팅방을 연결/생성할때 명시. )
    - 만약 상대와의 과거 채팅방이 없는 경우 채팅방을 새로 생성하고, 과거 채팅방이 있는 경우 과거 메세지들을 json List로 반환.

--- living-alone-springboot/src/main/java/com/livingalone/springboot/chat/entity/ChatRoom.java
+++ living-alone-springboot/src/main/java/com/livingalone/springboot/chat/entity/ChatRoom.java
@@ -4,12 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoom {
 
     @Id
